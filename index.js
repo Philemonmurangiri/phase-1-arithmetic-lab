ec674df806e2dd86a616013e93ddb88784a96bff
philemonmurangiri <murangirilesx890@gmail.com>
2024-03-22 19:26:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 4) + 1;
+console.log(random); 
+
+let num3 = 20;
+let num4 = 8;
+let mod = (num3 % num4);
+console.log(mod);
+
+
+let numbers = [5, 10, 15, 20, 17];
+let max = Math.max(...numbers);
+console.log(max); 
